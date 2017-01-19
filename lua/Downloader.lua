888df4f437cea2ab9346547dfa9e62efab7098b8
林志文 <lzw0760@175game.com>
2017-01-19 11:45:53 +0800

    完善功能

--- lua/Downloader.lua
+++ lua/Downloader.lua
@@ -1,32 +1,41 @@
 -- created by lzw @ 2017-1-18
--- 文件下载器
--- Resume模式需要修改 C++ LuaMinXmlHttpRequest::_sendRequest，原生的cocos代码将断点回传的206响应当做错误处理了
--- usage
+-- 功能：文件下载器
+-- 断点模式流程:
+-- step1. 发送 range:"bytes=0-0" 获取网络文件大小
+-- step2. 计算要获取的片段，然后检查ETag是否与本地文件一致，不一致重新下载
+-- step3. 写入文件，并判断文件是否已经写满，写满返回callback("success")
+
+-- Notice: Resume模式需要修改 C++ LuaMinXmlHttpRequest::_sendRequest，原生的cocos代码将断点回传的206响应当做错误处理了
+-- Usage: 七牛云测试通过
 --[[
 	local Downloader = require "Downloader"
 	local url = "http://ojlmoqu25.bkt.clouddn.com/network.lua"
-	local dw = Downloader:create(self, "f:/testDwn.lua", url, function(status)
-		print("Downloader status", status)
+	local dw = Downloader:create(self, "f:/testDwn.lua", url, function(status, percent)
+		print("Downloader status", status, percent or 0)
 	end)
 	-- dw:start()
-	dw:start(true) -- 断点续存模式
+	dw:start(true) -- true: 断点续存模式
 --]]
+
 local Downloader = class("Downloader", cc.Node)
 
 function Downloader:ctor(parent, savePath, url, callback)
 	self.url = url
-	self.totleTryTimes = 3
+	self.totleTryTimes = 3				-- 重试次数
 	self.tryTimes = self.totleTryTimes
-	self.tryInterval = 5
-	self.timeout = 3
+	self.tryInterval = 5 				-- 重试间隔s
+	self.timeout = 3 					-- http超时设置
 	self.savePath = savePath
-	self.rangeBytes = 1 * 1024-- * 1024
+	self.rangeBytes = 1 * 1024  	-- 1MB的分片大小
 	self.totleSize = nil
 	self.eTag = nil
 	self.isResume = false
 	self.callback = callback
 	parent:addChild(self)
 end
+
+--------------------------------------------------------------
+-- public
 
 function Downloader:start(isResume)
 	if not isResume then
@@ -40,8 +49,15 @@ function Downloader:start(isResume)
 	self.totleSize = nil
 	self.eTag = nil
 	self.isResume = isResume
-	self:download()
+	if self.isResume then
+		self:startAfterNetFileSize()
+	else
+		self:download()
+	end
 end
+
+--------------------------------------------------------------
+-- private
 
 function Downloader:mapHeaders(headers)
 	headers = string.split(headers, "\n")
@@ -85,6 +101,40 @@ function Downloader:httpRequest(method, url, callback, param)
 	xhr:send(param.data)
 end
 
+function Downloader:startAfterNetFileSize()
+	local range = "bytes=0-0"
+	local param = 
+	{
+		headers =
+		{
+			["Range"] = range,
+		},
+		timeout = self.timeout,
+	}
+	self:httpRequest("GET", self.url, function(data, headers)
+		if data == nil then
+			local func = handler(self, self.startAfterNetFileSize)
+			return self:tryAgain(func)
+		end
+
+		if not self:checkETag(headers, size) then
+			os.remove(self.savePath)
+			return self:start(self.isResume)
+		end
+
+		local wantSize = self:getFileSize(headers)
+		if wantSize == 0 then
+			return self:onSuccess()
+		end
+
+		local nowSize = io.filesize(self.savePath)
+		if wantSize <= nowSize then
+			return self:onSuccess()
+		end
+		self:download()
+	end, param)
+end
+
 function Downloader:download()
 	local range = nil
 	local size = io.filesize(self.savePath)
@@ -106,11 +156,11 @@ function Downloader:download()
 		},
 		timeout = self.timeout,
 	}
-
-	self.tryTimes = self.totleTryTimes
+	print(range)
 	self:httpRequest("GET", self.url, function(data, headers)
 		if data == nil then
-			return self:tryAgain()
+			local func = handler(self, self.download)
+			return self:tryAgain(func)
 		end
 
 		if not self:checkETag(headers, size) then
@@ -129,6 +179,8 @@ function Downloader:download()
 		if wantSize <= nowSize then
 			return self:onSuccess()
 		end
+		self.tryTimes = self.totleTryTimes
+		self:onProgress(nowSize, wantSize)
 		self:download()
 	end, param)
 end
@@ -151,8 +203,11 @@ function Downloader:getFileSize(headers)
 	if self.totleSize ~= nil then
 		return self.totleSize
 	end
+	self.totleSize = 0
 	local value = headers["Content-Range"]
-	self.totleSize = tonumber(string.match(value, "%d+$"))
+	if value ~= nil then
+		self.totleSize = tonumber(string.match(value, "%d+$"))
+	end
 	return self.totleSize
 end
 
@@ -171,17 +226,26 @@ function Downloader:readETagFile(savePath)
 	return self.eTag
 end
 
+function Downloader:onProgress(nowSize, wantSize)
+	local percent = 0
+	if wantSize ~= 0 then
+		percent = nowSize / wantSize * 100
+	end
+	self.callback("progress", percent)
+end
+
 function Downloader:onFail()
 	self.callback("fail")
 end
 
 function Downloader:onSuccess()
+	self:onProgress(1, 1)
 	local eTagTemp = self.savePath .. "temp"
 	os.remove(eTagTemp)
 	self.callback("success")
 end
 
-function Downloader:tryAgain()
+function Downloader:tryAgain(func)
 	self.tryTimes = self.tryTimes - 1
 	if self.tryTimes <= 0 then
 		return self:onFail()
